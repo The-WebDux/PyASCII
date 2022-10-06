c7fb694d724f087589ce3217f563476262e94125
WebDux <68965859+vakh0@users.noreply.github.com>
2022-10-07 00:10:02 +0400

    Add files via upload

---
+++ pyascii.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Coded by - WebDux -
+import pyfiglet
+import argparse
+fonts = ["banner", "big", "block", "bubble", "circle", "digital", "emboss", "emboss2", "future", "ivrit", "lean", "letter", "mini", "mnemonic", "pagga", "script", "shadow", "slant", "small", "smblock", "smbraille", "smscript", "smshadow", "smslant", "standard"]
+
+
+parser = argparse.ArgumentParser(description='text to ASCII art')
+parser.add_argument('-t','--text', type=str, nargs='+', action='append')
+args = parser.parse_args()
+
+
+try:
+	for textargs in args.text:
+		for textarg in textargs:
+			for font in fonts:
+		 		print(pyfiglet.figlet_format(textarg, font=font))
+except TypeError:
+	print("[ERROR] Give me argument: -t youe_text")
